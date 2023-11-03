fe2e6991c9ab93b52f8a2dfe005c3f82407841e0
wbosw <146214919+wbosw@users.noreply.github.com>
2023-11-03 22:14:42 +0000

    working on actors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,9 +6,9 @@ class DirectorsController < ApplicationController
   def show
     the_id = params.fetch("the_id") 
 
-    matching_records = Director.where({:id => the_id})
+    @matching_records = Director.where({:id => the_id})
 
-    @the_director = matching_records.at(0)
+    @the_director = @matching_records.at(0)
 
     render({:template => "director_templates/details"})
   end
